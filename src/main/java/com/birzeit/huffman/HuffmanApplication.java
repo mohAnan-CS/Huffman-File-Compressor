87d8e3cc51f23e48d7fb8ad9ae07ab360406431d
Mohammad Anan <smoker_shuger@hotmail.com>
2023-07-16 00:42:24 +0300

    Add button for browse file, compress and decompress

--- src/main/java/com/birzeit/huffman/HuffmanApplication.java
+++ src/main/java/com/birzeit/huffman/HuffmanApplication.java
@@ -11,9 +11,10 @@ public class HuffmanApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HuffmanApplication.class.getResource("huffman-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Huffman Compressor");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
--- src/main/java/com/birzeit/huffman/HuffmanController.java
+++ src/main/java/com/birzeit/huffman/HuffmanController.java
@@ -1,14 +1,54 @@
 package com.birzeit.huffman;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class HuffmanController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HuffmanController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TableView<?> huffmanTableView;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<?, ?> character;
+
+    @FXML
+    private TableColumn<?, ?> frequency;
+
+    @FXML
+    private TableColumn<?, ?> code;
+
+    @FXML
+    private TextField filePathText;
+
+    @FXML
+    void browseFileOnAction() {
+
+        System.out.println("Browsing ...");
+        filePathText.setText("C:\\Users\\user\\Desktop\\test.txt");
+
+    }
+
+    @FXML
+    void compressOnAction() {
+
+        System.out.println("Compressing ...");
+
+    }
+
+    @FXML
+    void decompressOnAction() {
+
+        System.out.println("Decompressing ...");
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
