1239e220ebc28e9f6fa85dff3978a9659731de1e
Mohammad Anan <smoker_shuger@hotmail.com>
2023-07-15 21:39:23 +0300

    Add bit writer on file

---
+++ src/main/java/com/birzeit/huffman/file/BitOutputStream.java
@@ -0,0 +1,20 @@
+package com.birzeit.huffman.file;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class BitOutputStream {
+
+    private FileOutputStream out;
+    public BitOutputStream(FileOutputStream is) {
+
+        this.out = is;
+    }
+
+    public void writeH(StringBuilder header) throws IOException {
+        for (int i = 0; i < header.length(); i++) {
+            out.write((byte) header.charAt(i));
+        }
+    }
+    
+}
